e0936ca14b92a692308c5b2b60d618cf901a1ae4
Aditya Bheke <adityabheke34@gmail.com>
2025-01-17 15:04:31 +0530

    close server if unable to connect to mongodb

--- CarPool_MERN/CarPool_Backend/index.js
+++ CarPool_MERN/CarPool_Backend/index.js
@@ -29,6 +29,7 @@ mongoose.connect(process.env.MONGODB_URI, {
     console.log('Connected to MongoDB');
 }).catch((error) => {
     console.error('MongoDB connection error:', error);
+    process.exit(1);
 });
 
 app.get('/', (req, res) => {
